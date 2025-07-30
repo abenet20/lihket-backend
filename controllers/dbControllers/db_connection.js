4d8ffef94d63bc09f9ba22929ddc826ab5a5027a
Abenet Erbelo <abeneterbelo@gmail.com>
2025-07-30 16:57:36 +0300

    Update db_connection.js

--- controllers/dbControllers/db_connection.js
+++ controllers/dbControllers/db_connection.js
@@ -10,7 +10,7 @@ const mysql = require("mysql2/promise");
 const database = mysql.createPool({
   port: "4000",
     user: "2D3iQN52LRKfD8s.root",
-    password : "",
+    password : "Wx5TlKNi5NAx50h4",
     host : "gateway01.us-west-2.prod.aws.tidbcloud.com",
     database : "lihket",
   ssl: {
