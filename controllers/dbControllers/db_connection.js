7f60b8fa88323adda8eb709192d534dea19ec435
Abenet Erbelo <abeneterbelo@gmail.com>
2025-07-30 11:04:30 +0300

    tidb connection

--- controllers/dbControllers/db_connection.js
+++ controllers/dbControllers/db_connection.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql2/promise");
 
-const database = mysql.createPool({
-     user: "lihket",
-    password : "lihket",
-    host : "localhost",
-    database : "lihket",
-});
-
 // const database = mysql.createPool({
-//   port: "33636",
-//     user: "lihket",
-//     password : "Y1890b?jk",
-//     host : "mysql-db03.remote:33636",
+//      user: "lihket",
+//     password : "lihket",
+//     host : "localhost",
 //     database : "lihket",
 // });
+
+const database = mysql.createPool({
+  port: "4000",
+    user: "2D3iQN52LRKfD8s.root",
+    password : "Y1890b?jk",
+    host : "gateway01.us-west-2.prod.aws.tidbcloud.com",
+    database : "lihket",
+});
 
 module.exports = database;
 
