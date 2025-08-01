1c0c83f4d574851fc9b457f9e4fd34189686ab9a
Abenet Erbelo <abeneterbelo@gmail.com>
2025-08-01 13:11:34 +0300

    added students list fro teachers attendance

---
+++ controllers/teachersControllers/studentsList.js
@@ -0,0 +1,37 @@
+const verifyToken = require("../../middleware/verifyToken.js");
+const { body, validationResult } = require("express-validator");
+const database = require("../dbControllers/db_connection.js");
+
+exports.attendanceStudentsList = [
+  verifyToken,
+  async (req, res) => {
+    const userId = req.user.id;
+    try {
+const [teacher] = await database.query(
+      `SELECT * FROM teachers WHERE user_id = ?`,
+      [userId]
+    );
+
+    const [classroom] = await database.query(
+      `SELECT * FROM classes WHERE teacher_id = ?`,
+      [teacher[0].id]
+    );
+      // Fetching students list
+      const [studentsList] = await database.query(
+        `SELECT * FROM students WHERE class_id = ?`,
+        [classroom[0].id]
+      );
+      if (studentsList.length === 0) {
+        return res.status(404).json({ message: "No students found in this class." });
+      }
+      res.status(200).json({
+        message: "Students list fetched successfully.",
+        students: studentsList,
+      });
+    } catch (error) {
+      console.error("Error fetching students list:", error);
+      res.status(500).json({ message: "Internal server error." });
+    }
+  }
+];
+         
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {attendance} = require('../controllers/teachersControllers/attendance');
 const { addExam, getExams, deleteExam } = require("../controllers/teachersControllers/exams");
+const { attendanceStudentsList } = require("../controllers/teachersControllers/studentsList");
 
 
 router.post('/attendance', attendance);
 router.post("/add-exam", addExam);
 router.post("/get-exams", getExams);
 router.post("/delete-exam", deleteExam);
+router.get("/attendance-students-list", attendanceStudentsList);
 
 module.exports = router;
